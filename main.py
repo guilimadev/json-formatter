b26341471e29afa465809faf3bf789c4bb5f7c26
guilimadev <guilimadev@gmail.com>
2023-11-30 10:11:13 -0300

    Retirada do erro

--- main.py
+++ main.py
@@ -3,72 +3,73 @@ import json
 
 f = st.file_uploader("Coloque o JSON")
 
-data = json.load(f)
+if f:
+  data = json.load(f)
 
-# Create the new JSON structure
-new_json = {
-    "evtTabRubrica": {
-        "ideEvento": {
-            "tpAmb": "2",
-            "procEmi": "1",
-            "verProc": "1.6.0"
-        },
-        "ideEmpregador": {
-            "tpInsc": "1",
-            "nrInsc": "08498701"
-        },
-        "infoRubrica": {
-            "inclusao": {
-                "ideRubrica": {
-                    "codRubr": data["ideRubrica"]["codRubr"],
-                    "ideTabRubr": data["ideRubrica"]["ideTabRubr"],
-                    "iniValid": data["ideRubrica"]["iniValid"],
-                    "fimValid": ""
-                },
-                "dadosRubrica": {
-                    "dscRubr": data["dadosRubrica"]["dscRubr"],
-                    "natRubr": data["dadosRubrica"]["natRubr"],
-                    "tpRubr": data["dadosRubrica"]["tpRubr"],
-                    "codIncCP": data["dadosRubrica"]["codIncCP"],
-                    "codIncIRRF": data["dadosRubrica"]["codIncIRRF"],
-                    "codIncFGTS": data["dadosRubrica"]["codIncFGTS"],
-                    "codIncCPRP": data["dadosRubrica"]["codIncCPRP"],
-                    "tetoRemun": data["dadosRubrica"]["tetoRemun"],
-                    "observacao": data["dadosRubrica"]["tetoRemun"],
-                    "ideProcessoCP": [
-                      {
-                        "tpProc": "",
-                        "nrProc": "",
-                        "extDecisao": "",
-                        "codSusp": ""
-                      }
-                    ],
-                    "ideProcessoIRRF": [
-                      {
-                        "nrProc": "",
-                        "codSusp": ""
-                      }
-                    ],
-                    "ideProcessoFGTS": [
-                      {
-                        "nrProc": ""
-                      }
-                    ]
-                }
-            }
-        }
-    }
-}
+  # Create the new JSON structure
+  new_json = {
+      "evtTabRubrica": {
+          "ideEvento": {
+              "tpAmb": "2",
+              "procEmi": "1",
+              "verProc": "1.6.0"
+          },
+          "ideEmpregador": {
+              "tpInsc": "1",
+              "nrInsc": "08498701"
+          },
+          "infoRubrica": {
+              "inclusao": {
+                  "ideRubrica": {
+                      "codRubr": data["ideRubrica"]["codRubr"],
+                      "ideTabRubr": data["ideRubrica"]["ideTabRubr"],
+                      "iniValid": data["ideRubrica"]["iniValid"],
+                      "fimValid": ""
+                  },
+                  "dadosRubrica": {
+                      "dscRubr": data["dadosRubrica"]["dscRubr"],
+                      "natRubr": data["dadosRubrica"]["natRubr"],
+                      "tpRubr": data["dadosRubrica"]["tpRubr"],
+                      "codIncCP": data["dadosRubrica"]["codIncCP"],
+                      "codIncIRRF": data["dadosRubrica"]["codIncIRRF"],
+                      "codIncFGTS": data["dadosRubrica"]["codIncFGTS"],
+                      "codIncCPRP": data["dadosRubrica"]["codIncCPRP"],
+                      "tetoRemun": data["dadosRubrica"]["tetoRemun"],
+                      "observacao": data["dadosRubrica"]["tetoRemun"],
+                      "ideProcessoCP": [
+                        {
+                          "tpProc": "",
+                          "nrProc": "",
+                          "extDecisao": "",
+                          "codSusp": ""
+                        }
+                      ],
+                      "ideProcessoIRRF": [
+                        {
+                          "nrProc": "",
+                          "codSusp": ""
+                        }
+                      ],
+                      "ideProcessoFGTS": [
+                        {
+                          "nrProc": ""
+                        }
+                      ]
+                  }
+              }
+          }
+      }
+  }
 
-# Convert the new JSON to a string
-new_json_string = json.dumps(new_json, indent=2)
+  # Convert the new JSON to a string
+  new_json_string = json.dumps(new_json, indent=2)
 
-# Print the result
-st.write(new_json_string)
+  # Print the result
+  st.write(new_json_string)
 
-st.download_button(
-    label="Download data as JSON",
-    data=new_json_string,
-    file_name='new_json_string.json',
-   
-)
+  st.download_button(
+      label="Download data as JSON",
+      data=new_json_string,
+      file_name='new_json_string.json',
+    
+  )
